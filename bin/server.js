872fd737b1caae2804cf39ed1e161c5bde7ec863
Julien Bouquillon <julien@revolunet.com>
2017-05-31 11:07:08 +0200

    server: simplify and work with http+https

--- bin/server.js
+++ bin/server.js
@@ -1,63 +1,58 @@
 var five = require("johnny-five"),
-    express = require("express"),
-    app = express(),
-    fs = require("fs"),
-    path = require("path"),
-    server = require('http').Server(app);
-
-app.use(express.static('public'));
+  express = require("express"),
+  app = express(),
+  fs = require("fs"),
+  path = require("path"),
+  server = require("http").Server(app)
 
 
+// socket.io server (real-time)
+var io = new require("socket.io")();
+
+// serve static files : html, js, css
+app.use(express.static("public"))
+
+// https
 var sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
-  passphrase: 'makery'
-};
-
-
-
-function notify(socket, value) {
-  socket.emit('pulse', value)
+  key: fs.readFileSync(path.join(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
+  passphrase: "makery"
 }
 
+// serve http
+var http = require("http").createServer(app).listen(8082, function() {
+  console.log("listening on http://127.0.0.1:8082")
+})
 
-var board = new five.Board();
+// serve https
+var https = require("https").createServer(sslOptions, app).listen(8443, function() {
+  console.log("listening on https://127.0.0.1:8443")
+})
+
+// use http+https for socket.io
+io.attach(http)
+io.attach(https)
+
+
+// johnny-five board autodetection
+var board = new five.Board()
 
 board.on("ready", function() {
+  board.info("Board", "ready")
 
-  board.info('Board', 'ready');
-
+  // declare our sensor
   var sensor = new five.Sensor({
     pin: "A0",
     freq: 10
-  });
+  })
 
-  var currentSocket;
+  // scale 0-1024 values to 0-100
+  sensor.scale([0, 100]).on("change", function() {
+    io.sockets.emit("pulse", this.scaled)
+  })
 
-  sensor.scale([ 0, 100 ]).on("change", function() {
-    if (currentSocket) {
-      notify(currentSocket, this.scaled);
-    }
-  });
+  io.on("connection", function(socket) {
+    board.info("socket.io", "incoming connection")
+  })
+})
 
-  io.on('connection', function(socket){
-
-    currentSocket = socket;
-
-    board.info('socket.io', 'connection');
-
-  });
-
-
-});
-
-
-var http = require('http').createServer(app).listen(8082, function() {
-    console.log('listening on http://127.0.0.1:8082');
-});
-
-var https = require('https').createServer(sslOptions, app).listen(8443, function() {
-    console.log('listening on https://127.0.0.1:8443');
-});
-
-var io = require('socket.io')(https);
